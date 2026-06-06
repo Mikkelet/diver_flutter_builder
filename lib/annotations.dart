a706d468d38b5918717ed0741dcefff6d0789092
Mikkel Thygesen <Mikkelet@gmail.com>
2026-06-06 22:49:57 +0200

    Remove `DiverRoute` annotation
    The `DiverRoute` annotation, which was introduced to provide human-readable `name` and `description` for `TypedGoRoute` classes in `app_urls.json`, is no longer used and has been removed.

--- lib/annotations.dart
+++
@@ -1,8 +0,0 @@
-/// Optional annotation attached to a go_router route class to enrich the
-/// generated `app_urls.json` with a human-readable [name] and [description].
-class DiverRoute {
-  const DiverRoute({required this.name, required this.description});
-
-  final String name;
-  final String description;
-}
