cb8afa6fb4c38da9907a96f4043bf64e7373ba3f
Mikkel Thygesen <Mikkelet@gmail.com>
2026-06-06 19:43:28 +0200

    Update dependencies and adapt to analyzer API
    Bumps the 'analyzer' package to '^8.2.0', which introduced breaking API changes.
    This update necessitates adjustments to how metadata annotations and constructor parameters are accessed (e.g., from 'element.metadata' to 'element.metadata.annotations' and 'constructor.parameters' to 'constructor.formalParameters').
    
    The 'source_gen' dependency's lower bound was also relaxed to '^3.0.0'.

--- lib/src/url_aggregator_builder.dart
+++ lib/src/url_aggregator_builder.dart
@@ -53,7 +53,7 @@ class UrlAggregatorBuilder implements Builder {
   }
 
   String? _readTypedGoRoutePath(Element element) {
-    for (final annotation in element.metadata) {
+    for (final annotation in element.metadata.annotations) {
       final annotationElement = annotation.element;
       final enclosingName =
           annotationElement?.enclosingElement?.name ?? annotationElement?.name;
@@ -68,7 +68,7 @@ class UrlAggregatorBuilder implements Builder {
 
   bool _hasExtraParameter(ClassElement element) {
     for (final constructor in element.constructors) {
-      for (final parameter in constructor.parameters) {
+      for (final parameter in constructor.formalParameters) {
         if (parameter.name == _extraParamName) return true;
       }
     }
